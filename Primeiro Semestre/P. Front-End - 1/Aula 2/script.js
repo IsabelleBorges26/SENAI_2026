f053760fb2ac6a30c13d213e5a2ed5a62f762f01
IsabelleBorges26 <manuborges2612@gmail.com>
2026-02-25 11:30:19 -0300

    Update

--- Primeiro Semestre/P. Front-End - 1/Aula 2/script.js
+++ Primeiro Semestre/P. Front-End - 1/Aula 2/script.js
@@ -1,12 +1,56 @@
-let clientes = JSON.parse(localStorage.getItem ("clientes")) || [];
+let clientes = JSON.parse(localStorage.getItem("clientes")) || [];
 
-document.addEventListener("DOMContentLoaded", renderizarTabela);
+document.addEventListener("DOMContentLoaded", () => {
+    renderizarTabela();
+
+    const cadCliente = document.getElementById("cadCliente");
+
+    cadCliente.addEventListener("submit", (formulario) => {
+        formulario.preventDefault();
+
+        const obj = {
+            cpf: cadCliente.cpf.value,
+            nome: cadCliente.nome.value,
+            sobrenome: cadCliente.sobrenome.value,
+            nascimento: cadCliente.nascimento.value
+        };
+
+        clientes.push(obj);
+        localStorage.setItem("clientes", JSON.stringify(clientes));
+
+        renderizarTabela();
+        fecharModal();
+        cadCliente.reset();
+    });
+});
 
 function abrirModal() {
-    document.getElementById("modal").style.display="block";
+    document.getElementById("modal").style.display = "block";
 }
 
 function fecharModal() {
-     document.getElementById("modal").style.display="none";
-     limparCampos();
+    document.getElementById("modal").style.display = "none";
+    limparCampos();
+}
+
+function renderizarTabela() {
+    const dados = document.getElementById("dados");
+    dados.innerHTML = "";
+
+    clientes.forEach((cliente, i) => {
+        dados.innerHTML += `
+            <tr>
+                <td>${cliente.cpf}</td>
+                <td>${cliente.nome}</td>
+                <td>${cliente.sobrenome}</td>
+                <td>${cliente.nascimento}</td>
+                <td><button onclick="excluir(${i})">Excluir</button></td>
+            </tr>`;
+    });
+}
+
+function excluir(indice) {
+    clientes.splice(indice, 1);
+    localStorage.setItem("clientes", JSON.stringify(clientes));
+    renderizarTabela();
 }
